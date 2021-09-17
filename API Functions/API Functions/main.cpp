70ce7e444252051a6f6a55f4702d4b71449112f9
GuyFox <noone@mail.ru>
2021-09-17 17:35:58 +0300

    lab1 done

--- API Functions/API Functions/main.cpp
+++ API Functions/API Functions/main.cpp
@@ -10,14 +10,16 @@
 #pragma execution_character_set("utf-8")
 #endif
 
-
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 2024
+#endif
 
 using namespace std;
 
 int string_count;
 set<int> string_numbers;
 DWORD dwDesAccess = GENERIC_READ | GENERIC_WRITE, dwFlgAndAttributes = 0;
-wchar_t * from,* to;
+string from, to;
 
 
 
@@ -64,13 +66,13 @@ bool CLI_CONFIG(int argc, char* argv[]) {
             continue;
         }
         if (((string)argv[i]).find("-") == -1 && !SET_FM) {
-            from = (wchar_t*)argv[i];
+            from = (string)argv[i];
            
             SET_FM = true;
             continue;
         }
         if (((string)argv[i]).find("-") == -1 && !SET_TO) {
-            to = (wchar_t*)argv[i];
+            to = (string)argv[i];
             SET_TO = true;
             continue;
         }
@@ -81,16 +83,36 @@ bool CLI_CONFIG(int argc, char* argv[]) {
 }
 
 
+void _buffer_init(char * BUFFER) {
+    for (int i = 0; i < BUFFER_SIZE; i++) BUFFER[i] = '\0';
+}
+
+
 int main(int argc, char * argv[])
 {
-    char buffer[1024];
-    HANDLE hInput, hOutput;
-    /*SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);*/
+    char
+        bufferFirst[BUFFER_SIZE],
+        bufferSecond[BUFFER_SIZE],
+        bufferResult[BUFFER_SIZE * 2];
+    HANDLE 
+        hFrom,
+        hTo,
+        hOutFile;
+    DWORD
+        brFrom,
+        brTo,
+        brRes;
+    int 
+        stringIndex = 0;
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
     if (!CLI_CONFIG(argc, argv))return -1;
-    cout << from << ' ' << to;
-    hInput = CreateFile(
-        from,
+    _buffer_init(bufferFirst);
+    _buffer_init(bufferSecond);
+    _buffer_init(bufferResult);
+    hFrom = CreateFileA(
+        from.c_str(),
         dwDesAccess,
         FILE_SHARE_READ,
         NULL,
@@ -98,16 +120,42 @@ int main(int argc, char * argv[])
         FILE_ATTRIBUTE_NORMAL,
         NULL
     );
-    hOutput = CreateFile(
-        to,
+    hTo = CreateFileA(
+        to.c_str(),
         dwDesAccess,
         FILE_SHARE_READ,
         NULL, 
         OPEN_ALWAYS,
+        FILE_ATTRIBUTE_NORMAL,
+        NULL
+    );                                                  
+    hOutFile = CreateFileA(
+        "result.txt",
+        dwDesAccess,
+        FILE_SHARE_READ,
+        NULL,
+        CREATE_ALWAYS,
         dwFlgAndAttributes,
         NULL
     );
-
+    ReadFile(hFrom, bufferFirst, BUFFER_SIZE, &brFrom, NULL);
+    ReadFile(hTo, bufferSecond, BUFFER_SIZE, &brTo, NULL);
+    for (int str = 1, j =0; bufferFirst[stringIndex] != '\0'; stringIndex++) {
+        if (string_numbers.find(str) != string_numbers.end()) {
+            for (; bufferFirst[stringIndex] != '\n'; j++) {
+                bufferResult[j] = bufferFirst[stringIndex++];
+            }
+            bufferResult[j++] = bufferFirst[stringIndex];
+            str++;
+            continue; 
+        }
+        if (bufferFirst[stringIndex] == '\n')str++;
+    }
+    for (int j = 0; bufferSecond[j] != '\0'; j++, stringIndex++) {
+        bufferResult[stringIndex] = bufferSecond[j];
+    }
+    WriteFile(hOutFile, bufferResult, stringIndex, NULL, NULL);
+    CloseHandle(hFrom); CloseHandle(hTo); CloseHandle(hOutFile);
     return 0;
 }
 
